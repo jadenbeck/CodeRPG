5e060538fa0027423982bad8c1e021806bde7392
aleph-nul <aleph0.null0@gmail.com>
2019-11-02 16:08:25 -0400

    all tutorials and user config

--- Bot/bot.js
+++ Bot/bot.js
@@ -1,3 +1,4 @@
+//var players;
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
@@ -12,26 +13,116 @@ var bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
+
+//var length;
+class DBUserDatabase
+{
+     //players;
+
+    constructor()
+    {
+        this.players = new Array();
+        this.length = this.players.length;
+    }
+    addPlayer(DBUsername)
+    {
+        this.players[this.players.length] = new DBUser(DBUsername, 1);;
+    }
+    levelUp(userID)
+    {
+        for (var i = 0; i < this.players.length; i++) 
+        {
+            if (this.players[i].getName() == userID)
+            {
+                this.players[i].incrementLevel();
+                break;
+            }
+            bot.sendMessage({
+                to: channelID,
+                message: userID + ' has leveled up!'
+            });
+        }
+    }
+    printAll(channelID)
+    {
+        for (var i = 0; i < this.players.length; i++) 
+        {
+            bot.sendMessage({
+                to: channelID,
+                message: this.players[i].getName() + ' --> Level: ' + this.players[i].getLevel()
+            });
+        }
+    }
+}
+
+class DBUser 
+{
+    constructor(name, level) 
+    {
+        this.name = name;
+        this.level = level;
+    }
+    getName()
+    {
+        return this.name;    
+    }
+    getLevel()
+    {
+        return this.level;
+    }
+    incrementLevel()
+    {
+        this.level++;
+    }
+}
+
+var playerDatabase = new DBUserDatabase();
+
+
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
+
 bot.on('message', function (user, userID, channelID, message, evt) {
+
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
     if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
         var cmd2 = args[1];
+
         args = args.splice(1);
         switch(cmd) {
             // !help
             case 'help':
+            {
                 bot.sendMessage({
                     to: channelID,
-                    message: 'List of commands:\n!help: list of commands \n!start: starts a round of the rpg \n!tutorial: explanations for simple python coding'
+                    message: 'List of commands:\n!help: list of commands \n!start: starts a round of the rpg'
                 });
+            }
+            break;
+            case 'configure':
+            {
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Hello, ' + user
+                });
+                playerDatabase.addPlayer(userID);
+            }    
+            break;
+            case 'levelup':
+            {
+                playerDatabase.levelUp(userID);
+            }
+            break;
+            case 'everyone':
+            {
+                playerDatabase.printAll(channelID);
+            }
             break;
 			//tutorial
 			case 'tutorial':

5b2deafde04ab9e51b266be815c588b210c079f7
Jaden Beck <jadenhunter3@gmail.com>
2019-11-03 03:18:34 -0500

    FINISHEDgit add bot.js ! 11 working levels

--- Bot/bot.js
+++ Bot/bot.js
@@ -201,7 +201,19 @@ class RunCode {
 	}
 	
 	runPython(code, level) {
-		code = code.substring(9,code.length-3).trim();
+		code = "ab83fb362bfi27fba3 = 0\n" + code.substring(9,code.length-3).trim();
+		var lines = code.split("\n");
+		for (var i = 0; i < lines.length; i++) {
+			if (lines[i].includes("for") || lines[i].includes("while")) {
+				lines.splice(i+1, 0, '    ab83fb362bfi27fba3 += 1');
+				lines.splice(i+2, 0, '    if (ab83fb362bfi27fba3 > 100):');
+				lines.splice(i+3, 0, '    \tbreak');
+			}
+		}
+		code = ""
+		for (var i = 0; i < lines.length; i++) {
+			code += lines[i] + '\n'
+		}
 		var fs = require('fs');
 		
 		if (level == 1 || level == 3 || level == 4) {
